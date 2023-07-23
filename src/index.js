06b147a9f971ca40dd4465e39dedc76332206ef2
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-07-23 18:12:02 +0300

    Making the component reusable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetRatingHandler={setMovieRating}
+      />
+      <p>
+        <strong style={{ fontSize: "22px" }}>
+          This movie was rated {movieRating} stars
+        </strong>
+      </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    {/* <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="red" />
+    <StarRating maxRating={5} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
